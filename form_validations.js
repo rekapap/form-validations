42bbbdfa21686795a468df904b868bdc745a8825
Reka <rekapap28@gmail.com>
2019-02-01 11:24:35 +0000

    adds emailValidate

--- form_validations.js
+++ form_validations.js
@@ -1,5 +1,19 @@
+function validateEmail(emailString){
+  if(emailString === "") return false;
+
+  let emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]{1,63}(?:\.([a-zA-Z0-9-]){1,63})+$/;
+  let match = emailRegExp.test(emailString);
+  if(!match) return false;
+
+  return true;
+}
+
 function validateForm() {
+  let mail = document.getElementById('mail');
   let formValidity = false;
+
+  if (validateEmail(mail.value)) formValidity = true;
+  console.log(formValidity);
 
   return formValidity;
 }
