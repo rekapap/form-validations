b4fe1d080ffb35928f2fe0cd0db4f0d2101f3070
Reka <rekapap28@gmail.com>
2019-02-01 11:22:03 +0000

    adds validate form

--- form_validations.js
+++ form_validations.js
@@ -1,3 +1,10 @@
+function validateForm() {
+  let formValidity = false;
+
+  return formValidity;
+}
+
 document.getElementsByTagName('form')[0].addEventListener('submit', function(event){
   event.preventDefault();
+  validateForm();
 });
